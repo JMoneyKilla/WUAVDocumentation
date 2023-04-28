c470d5c32df503aaee17328b61cbc9586c3ee683
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-04-28 13:16:17 +0200

    testing methods for builders

--- src/dal/DocumentDAO.java
+++ src/dal/DocumentDAO.java
@@ -1,5 +1,12 @@
 package dal;
 
-public class DocumentDAO {
+import be.documents.IDocument;
 
+import java.util.List;
+
+public class DocumentDAO {
+    DataBaseConnection dbc = DataBaseConnection.getInstance();
+    public List<IDocument> getAllProjectDocuments(int projectId){
+        return null;
+    }
 }
