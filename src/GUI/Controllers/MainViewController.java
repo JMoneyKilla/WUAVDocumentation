a2a8d9b7d41d1c83f2a525a31f085ce2268c7010
Madsp15 <madsp@hotmail.dk>
2023-05-01 12:11:51 +0200

    added managers

--- src/GUI/Controllers/MainViewController.java
+++ src/GUI/Controllers/MainViewController.java
@@ -1,13 +1,33 @@
 package GUI.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 
-public class MainViewController {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainViewController implements Initializable {
+    public BorderPane borderPane;
     @FXML
     private Label lableUserName;
     @FXML
     private CheckBox toggleAddress, toggleProjectName, toggleCompanyName, toggleCustomerName;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/GUI/Views/ProjectsView.fxml"));
+        System.out.println(fxmlLoader);
+        borderPane.getChildren().remove(borderPane.getCenter()); //remove existing fxml from center.
+        try {
+            borderPane.setCenter(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/WUAVDocumentation.java
+++ src/WUAVDocumentation.java
@@ -1,5 +1,20 @@
-public class WUAVDocumentation {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class WUAVDocumentation extends Application{
+    public static void main(String[] args) {
+            Application.launch();
+        }
+
+        @Override
+        public void start(Stage primaryStage) throws Exception {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/GUI/Views/MainView.fxml"));
+            Parent root = loader.load();
+            primaryStage.setScene(new Scene(root));
+            primaryStage.show();
+        }
     }
-}
+
